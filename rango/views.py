5aa08efe522b2ff06265a11bf29547c69ed922d4
Fabrizio Catinella <2322021@student.gla.ac.uk>
2020-01-20 23:09:32 +0000

    Modified Index Template and Fixed Reference on Index View

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from rango.models import Category
 def index(request):
 
     # Query the data for the top 5 liked categories
-    category_list = Category.objects.orderby('-likes')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
