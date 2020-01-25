eda00f66da713b1e8e2a9b34bd22e1cbad3b29a8
Fabrizio Catinella <2322021@student.gla.ac.uk>
2020-01-25 17:45:07 +0000

    Created User Login View

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.models import Page
 from rango.forms import CategoryForm, PageForm, UserForm,  UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 def index(request):
 
@@ -113,3 +114,26 @@ def register(request):
         'profile_form': profile_form,
         'registered': registered
     })
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled")
+        else:
+            print(f'Invalid login details: {username}, {password}')
+            return HttpResponse("Invalid Login details supplied.")
+
+    else:
+        return render(request, 'rango/login.html')
