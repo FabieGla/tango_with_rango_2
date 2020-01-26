15935cf01b0a692a433245fe24d81773f6711749
Fabrizio Catinella <2322021@student.gla.ac.uk>
2020-01-26 14:03:01 +0000

    Added logout functionality

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from rango.models import Page
 from rango.forms import CategoryForm, PageForm, UserForm,  UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -142,3 +142,8 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
